644dd9474768198cce90fb72f70df3fab1fdba80
mdnriyas17 <mdnriyas@gmail.com>
2024-04-30 16:12:06 +0530

    origin

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: "*",
   },
 });
 
